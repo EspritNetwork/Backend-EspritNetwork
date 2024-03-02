3d0bdec2bdac2c5c53a772462ce216e2845ffafe
onesBenRhaime <onesrhaime28@gmail.com>
2024-03-02 01:31:20 +0100

    branch 'ones ' pull 'main'

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-/*package express pour cree une application express nodejs */
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
 const bodyperser = require("body-parser");
 const config = require("./config/dbConnection.json");
 const cors = require("cors"); // Import cors middleware
-
+const dotenv = require("dotenv");
 
 //connect to bd
 mongoose
@@ -16,33 +15,34 @@ mongoose
 	.then(() => console.log("Connexion à MongoDB réussie !"))
 	.catch(() => console.log("Connexion à MongoDB échouée !"));
 
-//create an instance of the app 
+//create an instance of the app
 var app = express();
-
 
 // for enabling cors middelware
 app.use(cors());
-
-
+app.use(bodyperser.json());
 const testRouter = require("./routes/test");
 const questionRouter = require("./routes/question");
 const cvRouter = require("./routes/cv");
 const offreRouter = require("./routes/offre");
 const condidacyRouter = require("./routes/condidacy");
-const userRouter = require("./routes/user");
+const affiliationRouter = require("./routes/affiliation");
+const competenceRouter = require("./routes/competence");
+const departementRouter = require("./routes/departement");
+const userRouter = require("./routes/UserRoutes");
 
-app.use(bodyperser.json());
-
+app.use("/api/users", userRouter);
 
 app.use("/test", testRouter);
 app.use("/question", questionRouter);
 app.use("/cv", cvRouter);
 app.use("/offre", offreRouter);
 app.use("/condidacy", condidacyRouter);
-app.use("/user", userRouter);
+app.use("/affiliation", affiliationRouter);
+app.use("/competence", competenceRouter);
+app.use("/departement", departementRouter);
 
-
-
+dotenv.config();
 
 //server configuration
 const server = http.createServer(app);
